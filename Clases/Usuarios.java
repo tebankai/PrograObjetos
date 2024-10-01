b23be0fcee53839a09624da6cc3947f47f9706c6
Esteban Rojas <erojash@ucenfotec.ac.cr>
2024-09-30 20:35:24 -0600

    Clase usuario

---
+++ Clases/Usuarios.java
@@ -0,0 +1,74 @@
+public class Usuarios {
+    
+    private int cedula;
+    private String nombre;
+    private String direccion;
+    private int telefono;
+    private String correo_electronico;
+    private String tipo;
+    
+    
+    // Constructor
+    
+    public Usuarios (int cedula, String nombre, String direccion, int telefono, String correo_electronico, String tipo ){
+        
+       this.cedula=cedula;
+       this.nombre=nombre;
+       this.direccion=direccion;
+       this.telefono=telefono;
+       this.correo_electronico=correo_electronico;
+       this.tipo=tipo;
+    
+    }
+
+    public int getCedula() {
+        return cedula;
+    }
+
+    public void setCedula(int cedula) {
+        this.cedula = cedula;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public int getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(int telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getCorreo_electronico() {
+        return correo_electronico;
+    }
+
+    public void setCorreo_electronico(String correo_electronico) {
+        this.correo_electronico = correo_electronico;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+    
+    
+    
+}
